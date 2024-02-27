2947bf1302ca5edfb4e10feaef0d093d3b09d36f
Dohwan Kim <windboy2004@naver.com>
2024-02-27 21:20:41 +0900

    chore: 로그인 에러 잡기용7

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,11 @@
 import {
   RequestCookies,
   ResponseCookies,
-} from 'next/dist/server/web/spec-extension/cookies';
+} from 'next/dist/compiled/@edge-runtime/cookies';
 import { NextRequest, NextResponse } from 'next/server';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}`;
 let newResponse: Response;
-
-function applySetCookie(req: NextRequest, res: NextResponse): void {
-  // parse the outgoing Set-Cookie header
-  const setCookies = new ResponseCookies(res.headers);
-  // Build a new Cookie header for the request by adding the setCookies
-  const newReqHeaders = new Headers(req.headers);
-  const newReqCookies = new RequestCookies(newReqHeaders);
-  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
-  // set “request header overrides” on the outgoing response
-  NextResponse.next({
-    request: { headers: newReqHeaders },
-  }).headers.forEach((value, key) => {
-    if (
-      key === 'x-middleware-override-headers' ||
-      key.startsWith('x-middleware-request-')
-    ) {
-      res.headers.set(key, value);
-    }
-  });
-}
 
 const isUserLogged = async (headers: Headers) => {
   let isLogged = false;
@@ -88,16 +68,35 @@ const isUserLogged = async (headers: Headers) => {
   return isLogged;
 };
 
+function applySetCookie(req: NextRequest, res: NextResponse): void {
+  const resCookies = new ResponseCookies(res.headers);
+  const newReqHeaders = new Headers(req.headers);
+  const newReqCookies = new RequestCookies(newReqHeaders);
+
+  resCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
+
+  NextResponse.next({
+    request: { headers: newReqHeaders },
+  }).headers.forEach((value, key) => {
+    if (
+      key === 'x-middleware-override-headers' ||
+      key.startsWith('x-middleware-request-')
+    ) {
+      res.headers.set(key, value);
+    }
+  });
+}
+
 export async function middleware(request: NextRequest) {
-  const res = NextResponse.next();
-  const sid = request.cookies.get('sid')?.value;
-  if (!sid) {
-    const id = crypto.randomUUID();
-    const response = NextResponse.redirect(request.url);
+  const code = request.cookies.get('code')?.value;
+
+  if (!code) {
+    const res = NextResponse.redirect(request.url);
+    res.cookies.set('code', 'code value');
     applySetCookie(request, res);
-    response.cookies.set('sid', id);
-    return response;
+    return res;
   }
+
   const { headers } = request;
   const { pathname, origin } = request.nextUrl;
   const isLogged = await isUserLogged(headers);

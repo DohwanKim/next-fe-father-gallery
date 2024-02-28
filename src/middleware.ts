7dd300f8097da079276d9634f4cd0d1c75f160cf
Dohwan Kim <windboy2004@naver.com>
2024-02-28 21:04:35 +0900

    chore: 로그인 에러 잡기용 10

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
+import {
+  RequestCookies,
+  ResponseCookies,
+} from 'next/dist/server/web/spec-extension/cookies';
 import { NextRequest, NextResponse } from 'next/server';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_API_URL}`;
@@ -64,7 +68,29 @@ const isUserLogged = async (headers: Headers) => {
   return isLogged;
 };
 
+function applySetCookie(req: NextRequest, res: NextResponse): void {
+  // parse the outgoing Set-Cookie header
+  const setCookies = new ResponseCookies(res.headers);
+  // Build a new Cookie header for the request by adding the setCookies
+  const newReqHeaders = new Headers(req.headers);
+  const newReqCookies = new RequestCookies(newReqHeaders);
+  setCookies.getAll().forEach((cookie) => newReqCookies.set(cookie));
+  // set “request header overrides” on the outgoing response
+  NextResponse.next({
+    request: { headers: newReqHeaders },
+  }).headers.forEach((value, key) => {
+    if (
+      key === 'x-middleware-override-headers' ||
+      key.startsWith('x-middleware-request-')
+    ) {
+      res.headers.set(key, value);
+    }
+  });
+}
+
 export async function middleware(request: NextRequest) {
+  const res = NextResponse.next();
+  applySetCookie(request, res);
   const { headers } = request;
   const { pathname, origin } = request.nextUrl;
   const isLogged = await isUserLogged(headers);

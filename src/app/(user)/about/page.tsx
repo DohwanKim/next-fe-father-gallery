76ef554312ecf538adb8f42767787bf94325a855
Dohwan Kim <windboy2004@naver.com>
2024-03-01 20:14:17 +0900

    chore: 메인 페이지를 랜딩 페이지로 활용하기 위해 비워둠

--- src/app/(user)/about/page.tsx
+++ src/app/(user)/about/page.tsx
@@ -1,5 +1,7 @@
 export default function Intro() {
   return (
-    <div className={'container'}>소개 페이지 - 나의 인생과 그림 (추후정리)</div>
+    <div className={'container'}>
+      [Static] 소개 페이지 - 나의 인생과 그림 (추후정리)
+    </div>
   );
 }

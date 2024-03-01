2376a866d141410a3e53b0fdda68b80027c64beb
Dohwan Kim <windboy2004@naver.com>
2024-03-01 20:12:56 +0900

    style: 스크롤시 헤더 감추기 기능 추가

--- src/components/user/organism/UserHeader.tsx
+++ src/components/user/organism/UserHeader.tsx
@@ -1,16 +1,60 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import ThemeToggleButton from '@/components/common/atom/ThemeToggleButton';
 import HeaderNav from '@/components/user/organism/HeaderNav';
 import HeaderSocial from '@/components/user/organism/HeaderSocial';
 
 const UserHeader = () => {
+  const pathname = usePathname();
+  const headerRef = useRef<HTMLDivElement>(null);
+  const lastScrollY = useRef(0);
+  const [isHeaderHideByScroll, setIsHeaderHideByScroll] =
+    useState<boolean>(false);
+
+  const mainScrollEvent = useCallback(() => {
+    const $header = headerRef.current;
+
+    if ($header) {
+      const minValue = 122;
+      const currentScrollY = window.scrollY;
+      if (minValue < currentScrollY) {
+        const isHide = lastScrollY.current < currentScrollY;
+
+        setIsHeaderHideByScroll(isHide);
+      }
+      lastScrollY.current = window.scrollY;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (window) {
+      window.addEventListener('scroll', mainScrollEvent);
+      mainScrollEvent();
+    }
+    return () => {
+      if (window) {
+        window.removeEventListener('scroll', mainScrollEvent);
+      }
+    };
+  }, [mainScrollEvent]);
+
+  useEffect(() => {
+    setIsHeaderHideByScroll(false);
+  }, [pathname]);
+
   return (
     <header
-      className={
-        'sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'
-      }
+      ref={headerRef}
+      className={`sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 ${
+        isHeaderHideByScroll
+          ? 'transform translate-y-[-100%]'
+          : 'transform translate-y-0'
+      } transition-transform duration-300 ease-in-out`}
     >
       <div
         className={

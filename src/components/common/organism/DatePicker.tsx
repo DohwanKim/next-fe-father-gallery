a4e7b3b9eeaa9d567709477f13b84ab61ea61e6d
Dohwan Kim <windboy2004@naver.com>
2024-03-12 18:22:05 +0900

    feat: 달력 플러그인에 셀렉트로 년도와 월을 고를 수 있게 커스텀마이징

--- src/components/common/organism/DatePicker.tsx
+++ src/components/common/organism/DatePicker.tsx
@@ -2,24 +2,84 @@
 
 import { format } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
-import { DateFormatter } from 'react-day-picker';
+import { DateFormatter, useNavigation } from 'react-day-picker';
 
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
 import { cn } from '@/lib/utils';
+import { numberPadding } from '@/utils/common';
+import dayjs from '@/utils/dayjs';
 
 interface Props {
   onChange: (date: Date | undefined) => void;
   value: Date;
 }
 
-const formatCaption: DateFormatter = (month, options) => {
-  return <>{format(month, 'yyyy년 MM월', { locale: options?.locale })}</>;
+const FormatCaption: DateFormatter = (month) => {
+  const { goToDate } = useNavigation();
+  const todayYear = Number(dayjs().format('YYYY'));
+  const currentYear = Number(format(month, 'yyyy'));
+  const currentMonth = Number(format(month, 'MM'));
+  const years = Array.from({ length: 10 }, (_, index) => todayYear + 1 - index);
+  const months = Array.from({ length: 12 }, (_, index) => index + 1);
+
+  return (
+    <>
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={'px-1 border hover:text-foreground/70 transition-colors'}
+          >
+            {currentYear}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {years.map((year) => (
+              <DropdownMenuItem
+                key={year}
+                onClick={() => {
+                  goToDate(new Date(Number(year), currentMonth - 1));
+                }}
+              >
+                {year}년
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <span className={'mr-2'}>년</span>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={'px-1 border hover:text-foreground/70 transition-colors'}
+          >
+            {numberPadding(currentMonth, 2)}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {months.map((month) => (
+              <DropdownMenuItem
+                key={month}
+                onClick={() => {
+                  goToDate(new Date(Number(currentYear), month - 1));
+                }}
+              >
+                {month}월
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <span>월</span>
+      </div>
+    </>
+  );
 };
 
 const DatePicker = ({ onChange, value }: Props) => {
@@ -43,7 +103,7 @@ const DatePicker = ({ onChange, value }: Props) => {
           selected={value}
           onSelect={onChange}
           initialFocus
-          formatters={{ formatCaption }}
+          formatters={{ formatCaption: FormatCaption }}
         />
       </PopoverContent>
     </Popover>

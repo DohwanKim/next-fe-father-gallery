f0e00984aae149690a2bdfa9d34e23ed4223fe6c
Dohwan Kim <windboy2004@naver.com>
2024-03-21 20:25:41 +0900

    test: 테스팅 작성에 어려움이 많아 Posts.tsx테스팅은 랜더로 일단락

--- src/components/user/organism/PostItemSkeleton.tsx
+++ src/components/user/organism/PostItemSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export const PostItemSkeleton = () => {
   return Array.from({ length: 9 }, (_, index) => (
-    <div key={index}>
+    <div key={index} data-testid={`skeleton-${index}`}>
       <Skeleton className="w-full aspect-square drop-shadow mb-5" />
       <div className={'flex flex-col items-center'}>
         <Skeleton className="h-4 w-[200px] mb-2" />
